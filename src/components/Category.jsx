f533e032f872053ae13992837f7432cd659ffc4e
M Abu Baker <abu.baker_e@hotmail.com>
2020-12-31 19:52:03 +0500

    initial commit

--- src/components/Category.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Typography } from '@material-ui/core'; 
-export default function Category(props) {
-    return ( <Typography variant="h6" color="textSecondary" gutterBottom>
-                {props.text.toString().replace(/%20/g,' ')
-                            .replace(/%3A/,':').replace(/%26/,'&')}
-            </Typography>
-    )
-}
